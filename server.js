3af67c6a1ce2c501ead7208b01ecd39d8ca3bd45
Evgen <e.salyakin@gmail.com>
2017-04-10 10:53:33 +0300

    prepare for deploy on Heroku

--- server.js
+++ server.js
@@ -3,40 +3,40 @@ var db = require('./server/db');
 var parser = require('body-parser');
 var server = express();
 
-server.use(parser.urlencoded());
-server.use(parser.json());
+var port = (process.env.PORT || '3000');
+var uristring =
+    process.env.MONGODB_URI ||
+    'mongodb://localhost:27017/todo-db';
 
+//server.use(parser.urlencoded());
+server.use(parser.json());
 server.use(express.static('./client'));
 
 server.post('/load', function (request, response) {
-
     let col = db.get().collection('records');
     let data;
     col.find().toArray(function (err,doc) {
         data = doc;
         response.json(data);
     });
-
 });
 
 server.post('/save', function (request, response) {
     data = request.body;
-
     let col = db.get().collection('records');
     col.remove({});
     col.insert(data);
-
     response.json({status:'ok'});
 });
 
 // Connect to Mongo on start
-db.connect('mongodb://localhost:27017/todo-db', function(err) {
+db.connect(uristring, function(err) {
     if (err) {
-        console.log('Unable to connect to Mongo.');
+        console.log('Unable to connect to Mongo. Use command: "sudo service mongod start" ');
         process.exit(1);
     } else {
-        server.listen(3000, function() {
-            console.log('Server started! Listening on port 3000...');
+        server.listen(port, function() {
+            console.log('Server started! Listening on port ',port);
         })
     }
 });
